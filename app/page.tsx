778428a87691368afc8fe3a25053d3e3fead238b
yash <yash.parakh.jain.23@gmail.com>
2025-07-16 01:30:34 +0530

    Added resume button

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import {
   title,
   desc_before_experience,
   desc_after_experience,
+  linkedIn,
+  github,
+  resumne_gdrive_link,
 } from "@/data/me";
-import { LinkedIn, Github } from "@/components/icons";
+import { LinkedIn, Github, Download } from "@/components/icons";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -24,8 +28,8 @@ export default function Home() {
       flex flex-col sm:pt-0 sm:ps-8 sm:h-full sm:justify-center items-center"
       >
         <h1 className="text-5xl font-bold ">{name}</h1>
-        <h2 className="text-3xl text-gray-700">{title}</h2>
-        <p className="mt-4 max-w-100 text-gray-600 text-lg text-justify">
+        <h2 className="text-3xl text-foreground/64">{title}</h2>
+        <p className="mt-4 max-w-100 text-foreground/64 text-lg text-justify">
           {desc_before_experience}
           <b>
             <Experience />
@@ -33,19 +37,16 @@ export default function Home() {
           {desc_after_experience}
         </p>
         <div className="flex flex-row gap-4 mt-6 justify-center items-center">
-          <a
-            href="https://www.linkedin.com/in/jainyash23/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href={linkedIn} target="_blank" rel="noopener noreferrer">
             <LinkedIn className="text-foreground w-6 h-6 hover:text-foreground/70 transition duration-400" />
           </a>
-          <a
-            href="https://github.com/yash-parakh"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href={github} target="_blank" rel="noopener noreferrer">
             <Github className="text-foreground w-8 h-8 hover:text-foreground/70 transition duration-400" />
+          </a>
+          <a href={resumne_gdrive_link}>
+            <Button variant={"outline"} className="inline-flex">
+              <Download /> Resume
+            </Button>
           </a>
         </div>
       </div>
--- data/me.js
+++ data/me.js
@@ -5,3 +5,6 @@ export const desc_before_experience = 'I love building software that makes a dif
 export const desc_after_experience = " years of industry experience in software development."; 
 export const email = "yashparakh234@gmail.com";
 export const location = "Madhya Pradesh, India";
+export const resumne_gdrive_link = "https://drive.usercontent.google.com/u/0/uc?id=1ehdU9x3DJTZVZMEyN7imvSwkYwCp3vRn&export=download";
+export const linkedIn = "https://www.linkedin.com/in/jainyash23/";
+export const github = "https://github.com/yash-parakh";
