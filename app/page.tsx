2d9b32a6d3b983f003db0c09d4eb88dbed8f7097
Yash <parakhyash23@gmail.com>
2025-08-29 23:48:28 +0530

    liter off

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,17 @@ import {
   github
 } from "@/data/me";
 import { LinkedIn, Github } from "@/components/icons";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row  h-full ps-4 pe-4 overflow-y-auto  justify-center items-center gap-6 md:gap-12 lg:gap-16">
-      <Image src={"/yash.jpg"}
+      <img
+        src="yash.jpg"
         alt="Yash Parakh"
-        width={300}
-        height={300}
-        className="w-80 h-80 rounded-full
+        className=" w-80 h-80 rounded-full
          md:w-90 md:h-90 lg:w-100 lg:h-100
         object-cover object-top overflow-hidden"
-      />
+      /> 
 
       <div
         className=" 
