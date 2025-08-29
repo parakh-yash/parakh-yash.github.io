713915e4ac100d97589b980e1fca5aeed9c4503b
Yash <parakhyash23@gmail.com>
2025-08-30 00:06:28 +0530

    Added loading animation on resumer page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,9 +1,26 @@
-
+'use client';
+import { useState } from 'react';
+import{
+resumne_link
+} from "@/data/me";
 export default function ResumePage() {
+  const [isLoading, setIsLoading] = useState(true);
+  
   return (
-    <iframe 
-        src="https://1drv.ms/w/c/9fd63a325bcbdc9c/IQSgFOx5g9JMTp71vy7tx2fqAbbcoNdS32ohRAamvtNo9RE" 
-        className="w-full h-full border-0">
-    </iframe>
+    <>
+      {/* Loader Overlay */}
+      {isLoading && (
+        <div className="absolute inset-0 z-10 flex items-center justify-center bg-white/70 backdrop-blur-sm">
+          <div className="h-12 w-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+        </div>
+      )}
+      <iframe 
+          src= {resumne_link}
+          className="w-full h-full border-0"
+          loading="lazy"
+          title="Resume"
+          onLoad={() => setIsLoading(false)}
+          />
+    </>
   );
 }
--- data/me.js
+++ data/me.js
@@ -5,6 +5,6 @@ export const desc_before_experience = 'I love building software that makes a dif
 export const desc_after_experience = " years of industry experience in software development."; 
 export const email = "parakhyash23@gmail.com";
 export const location = "Madhya Pradesh, India";
-export const resumne_gdrive_link = "https://onedrive.live.com/personal/9fd63a325bcbdc9c/_layouts/15/download.aspx?UniqueId=79ec14a0%2Dd283%2D4e4c%2D9ef5%2Dbf2eedc767ea";
+export const resumne_link = "https://1drv.ms/w/c/9fd63a325bcbdc9c/IQSgFOx5g9JMTp71vy7tx2fqAbbcoNdS32ohRAamvtNo9RE";
 export const linkedIn = "https://www.linkedin.com/in/parakhyash/";
 export const github = "https://github.com/parakh-yash";
